64ac29840067108b825227eb4610db2348e838c8
Eru <eru.gamecreater@gmail.com>
2023-07-07 13:53:28 +0900

    BulletController作成

---
+++ RoomHack.ver1.0/Assets/Eru/Scripts/Bullet/BulletController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class BulletController : MonoBehaviour,IUnitDamage
+{
+    public float maxHP { get; set; }
+    public float nowHP { get; set; }
+    public int dmgLayer { get; set; }
+
+    void Start()
+    {
+        maxHP = 1;
+        nowHP = maxHP;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.TryGetComponent<IUnitDamage>(out var damage))
+        {
+            if(this.dmgLayer != damage.dmgLayer)
+            {
+                damage.HitDmg();
+                this.HitDmg();
+            }
+        }
+    }
+
+    public void HitDmg()
+    {
+        nowHP--;
+        if (nowHP <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
