bdad59497cb01a0c72c3fab6bbde6f1ea12e7035
DESKTOP-67MT2K8\user <oktakeru@icloud.com>
2023-07-14 11:43:28 +0900

    画面表示についていくつか更新 by koko

---
+++ RoomHack.ver1.0/Assets/koko`s folder/Scripts/cctv.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cctv : MonoBehaviour, IUnitHack
+{
+    public bool hacked { get; set; } = false;
+
+    GameObject mosic;
+    public GameObject mosicPre;
+
+    public Vector2 areaPos;
+
+    private void Start()
+    {
+        mosic = transform.GetChild(0).gameObject;
+
+        GameObject temp = (GameObject)Instantiate(mosicPre, this.transform.position, Quaternion.identity);
+        temp.transform.parent = mosic.transform;
+
+        Vector2 length;
+        length.x = Mathf.Abs(areaPos.x);
+        length.y = Mathf.Abs(areaPos.y);
+
+        Vector2 startPos = this.transform.position;
+
+
+        for (int i = 0; i < length.x; i++)
+        {
+            for (int j = 0; j < length.y; j++)
+            {
+
+            }
+        }
+    }
+
+    public void StatusDisp()
+    {
+
+    }
+
+}
