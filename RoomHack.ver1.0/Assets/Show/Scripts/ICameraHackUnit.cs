36fed344a3201a4b165ab70b7f8c7da9837ed249
DESKTOP-67MT2K8\user <showjapan@icloud.com>
2023-07-18 18:14:11 +0900

    a版まで、たけると合わせます。　by show

--- RoomHack.ver1.0/Assets/Show/Scripts/ICameraHackUnit.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface ICameraHackUnit
-{
-    public bool CmHacked { get; set;}
-}
--- RoomHack.ver1.0/Assets/Show/Scripts/IDoorHackUnit.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IDoorHackUnit
-{
-    public bool DrHacked { get; set;}
-}
