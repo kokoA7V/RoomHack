58f119e24dd4bfa97837789d7a25ef8654759f56
Eru <eru.gamecreater@gmail.com>
2023-07-21 10:05:51 +0900

    少し修正

--- RoomHack.ver1.0/Assets/Show/CmScripts/CmUnhackImage.cs
+++ RoomHack.ver1.0/Assets/Show/CmScripts/CmUnhackImage.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CmUnHackImage : MonoBehaviour
+public class CmUnhackImage : MonoBehaviour
 {
     GameObject cmbuttonObj;
     CmButtonController cc;
